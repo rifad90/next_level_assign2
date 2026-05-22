f669e4aacd4a13318595eecc451da754518df1d8
RK <reshmy@Reshmys-MacBook-Air.local>
2026-05-22 23:12:04 +0600

    t #
    test

--- src/modeule/auth/auth.controller.ts
+++ src/modeule/auth/auth.controller.ts
@@ -39,9 +39,5 @@ export const authController = {
 
 
 
-// CONNECTIONSTRING = "postgresql://neondb_owner:npg_nCe5iYWK9yxk@ep-weathered-band-apj6qxr2-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
 
-// PORT = 3000
-
-// SECRET = "dsdsdsdsdsdgfgfgrrrrr"
 
